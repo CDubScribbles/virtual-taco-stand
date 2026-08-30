f22d182c7f6af0fd4c2cea4cc4dc07a50812d952
EMFDeathPenguin <cliffordwsmith@gmail.com>
2026-08-30 16:17:54 -0400

    Initial commit: Virtual Taco Stand through Chapter 4

---
+++ src/app/app.config.ts
@@ -0,0 +1,13 @@
+import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { GlobalErrorHandler } from './global-error-handler';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: ErrorHandler, useClass: GlobalErrorHandler }
+  ]
+};
